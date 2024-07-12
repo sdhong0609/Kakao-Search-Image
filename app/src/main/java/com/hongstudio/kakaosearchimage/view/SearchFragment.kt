731d40ac054a3c3055f522afff2fc4192894ae06
hong <sdhong0609@gmail.com>
2024-07-12 12:25:23 +0900

    fix: Retrofit build 싱글톤

--- app/src/main/java/com/hongstudio/kakaosearchimage/view/SearchFragment.kt
+++ app/src/main/java/com/hongstudio/kakaosearchimage/view/SearchFragment.kt
@@ -8,18 +8,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.hongstudio.kakaosearchimage.BuildConfig
 import com.hongstudio.kakaosearchimage.R
 import com.hongstudio.kakaosearchimage.base.BaseFragment
-import com.hongstudio.kakaosearchimage.common.DefaultJson
 import com.hongstudio.kakaosearchimage.database.DocumentDatabase
 import com.hongstudio.kakaosearchimage.databinding.FragmentSearchBinding
 import com.hongstudio.kakaosearchimage.model.Document
 import com.hongstudio.kakaosearchimage.model.GetSearchedImagesResponse
-import com.hongstudio.kakaosearchimage.service.SearchImageService
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import okhttp3.MediaType.Companion.toMediaType
+import com.hongstudio.kakaosearchimage.service.RetrofitObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
 import kotlin.concurrent.thread
 
 class SearchFragment : BaseFragment(R.layout.fragment_search) {
@@ -53,12 +49,7 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
     private fun getSearchedImages(keyword: String) {
         if (keyword.isBlank()) return
 
-        val contentType = "application/json".toMediaType()
-        val retrofit = Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(DefaultJson.asConverterFactory(contentType))
-            .build()
-        val service = retrofit.create(SearchImageService::class.java)
-        service.getSearchedImages(BuildConfig.REST_API_KEY, keyword)
+        RetrofitObject.searchImageService.getSearchedImages(BuildConfig.REST_API_KEY, keyword)
             .enqueue(object : Callback<GetSearchedImagesResponse> {
                 override fun onResponse(
                     call: Call<GetSearchedImagesResponse>,
