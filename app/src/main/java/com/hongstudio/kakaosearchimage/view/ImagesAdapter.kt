d128c4ae4d8397a808d12280465405c2fc8d38be
hong <sdhong0609@gmail.com>
2024-07-17 16:26:24 +0900

    fix: ListAdapter로 수정

--- app/src/main/java/com/hongstudio/kakaosearchimage/view/ImagesAdapter.kt
+++ app/src/main/java/com/hongstudio/kakaosearchimage/view/ImagesAdapter.kt
@@ -2,13 +2,14 @@ package com.hongstudio.kakaosearchimage.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.hongstudio.kakaosearchimage.databinding.ItemSearchedBinding
 import com.hongstudio.kakaosearchimage.model.Document
 
 class ImagesAdapter(
     private val onClickFavorite: (data: Document, position: Int) -> Unit
-) : RecyclerView.Adapter<ItemSearchedViewHolder>() {
+) : ListAdapter<Document, ItemSearchedViewHolder>(diffCallback) {
 
     private var dataSet: List<Document> = listOf()
 
@@ -24,15 +25,28 @@ class ImagesAdapter(
 
     override fun getItemCount() = dataSet.size
 
+    companion object {
+        private val diffCallback: DiffUtil.ItemCallback<Document> = object : DiffUtil.ItemCallback<Document>() {
+            override fun areItemsTheSame(oldDocument: Document, newDocument: Document): Boolean {
+                return oldDocument.thumbnailUrl == newDocument.thumbnailUrl
+            }
+
+            override fun areContentsTheSame(oldDocument: Document, newDocument: Document): Boolean {
+                return oldDocument == newDocument
+            }
+
+        }
+    }
+
     fun setData(dataSet: List<Document>) {
         this.dataSet = dataSet
-        notifyDataSetChanged()
+        submitList(dataSet)
     }
 
     fun setUpdatedDocument(document: Document, position: Int) {
         dataSet = dataSet.toMutableList().apply {
             set(position, document)
-        }.toList()
-        notifyItemChanged(position)
+        }
+        submitList(dataSet)
     }
 }
