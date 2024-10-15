32e45357790e9e2daaee49e6260fab791250e2ad
Seongdeok Hong <sdhong0609@gmail.com>
2024-10-15 17:31:34 +0900

    fix: loading 로직 분리

--- core/ui/src/main/java/com/hongstudio/ui/base/BaseViewModel.kt
+++ core/ui/src/main/java/com/hongstudio/ui/base/BaseViewModel.kt
@@ -3,9 +3,10 @@ package com.hongstudio.ui.base
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
@@ -14,7 +15,9 @@ abstract class BaseViewModel : ViewModel(), CoroutineScope {
     final override val coroutineContext: CoroutineContext = viewModelScope.coroutineContext
 
     private val _isLoading = MutableStateFlow(false)
-    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    @OptIn(FlowPreview::class)
+    val isLoading: Flow<Boolean> = _isLoading.debounce(700L)
 
     fun launchWithLoading(block: suspend CoroutineScope.() -> Unit) {
         launch {
